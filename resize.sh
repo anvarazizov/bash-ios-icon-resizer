41aff6418d9ad25a4da5e025e1acfde574eeaa00
Anvar Azizov <anvardejim@gmail.com>
2015-05-20 15:11:29 +0300

    First commit

---
+++ resize.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Resize script for $1 started"
+
+mkdir $1_icons
+cp $1 $1_icons
+cd $1_icons
+
+convert $1 -resize 512x512 "icon_512x512.png"
+convert $1 -resize 180x180 "icon_60x60@3x.png"
+convert $1 -resize 120x120 "icon_60x60@2x.png"
+convert $1 -resize 60.60 "icon_60x60.png"
+convert $1 -resize 120x120 "icon_40x40@3x.png"
+convert $1 -resize 80x80 "icon_40x40@2x.png"
+convert $1 -resize 40x40 "icon_40x40.png"
+convert $1 -resize 87x87 "icon_29x29@3x.png"
+convert $1 -resize 58x58 "icon_29x29@2x.png"
+convert $1 -resize 29x29 "icon_29x29.png"
+
+echo "Resisizg finished"
+open .
